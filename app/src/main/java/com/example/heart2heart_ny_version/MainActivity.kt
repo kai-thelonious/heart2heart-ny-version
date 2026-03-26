f36a9aef41bd0fde192567d0b58cc31e458c0fe2
Kai <kaibomholtz@gmail.com>
2026-03-26 10:59:20 +0100

    help

--- app/src/main/java/com/example/heart2heart_ny_version/MainActivity.kt
+++ app/src/main/java/com/example/heart2heart_ny_version/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.heart2heart_ny_version.ui.theme.Heart2heartnyversionTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,18 +25,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            Main()
         }
     }
 }
 
 @Composable
-fun Main() {
+fun Main(
+) {
+    LazyColumn(
+        modifier = Modifier
+        .padding(40.dp)
+        ,
 
+        ) {
+        item() {
+            Text("Brevkasse")
+        }
+        item() {
+            Text("Ugens ekspert")
+        }
+        item() {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .background(color = Color.Red)
+            )
+        }
+    }
 }
 
-@Preview
-@Composable
-fun Preview() {
-    Main()
-}
+//@Preview
+//@Composable
+//fun Preview() {
+//    Main()
+//}
