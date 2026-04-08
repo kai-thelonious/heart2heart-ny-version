d84d45e5f45596d6e660de945959a1ca5da541f5
victorbischoff <morkebergv@gmail.com>
2026-04-08 10:25:36 +0200

    Initial concept v4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.heart2heart_ny_version"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
